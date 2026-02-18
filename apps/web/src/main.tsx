a0a47a5d2dcb8b0fcc13e901e6650822001eafd9
suijiafneg <suijiafeng@hotmail.com>
2026-02-18 20:59:50 +0800

    feat(web): Vite + React 19 应用骨架

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>智能招聘平台建设中</div>
+  </StrictMode>,
+);
