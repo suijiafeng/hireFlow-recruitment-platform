6afcc115469b99146e809f017ee5c8cb0f726a67
suijiafneg <suijiafeng@hotmail.com>
2026-02-18 21:59:02 +0800

    chore(web): 接入 antd v6 与中文 locale

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,18 @@
+import { App as AntApp, Card, ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+dayjs.locale('zh-cn');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>智能招聘平台建设中</div>
+    <ConfigProvider locale={zhCN}>
+      <AntApp>
+        <Card style={{ margin: 24 }}>智能招聘平台建设中</Card>
+      </AntApp>
+    </ConfigProvider>
   </StrictMode>,
 );
